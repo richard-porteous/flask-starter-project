1356a3c6654b2f7079b635fec5190de06e9663ba
Richard Porteous <rich.porteous@gmail.com>
2023-02-14 15:06:12 -0700

    first commit of starter code

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
---
+++ app/main/routes.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app.main import bp
+
+@bp.route('/')
+@bp.route('/index')
+def index():
+    user = {'username': 'Bob'}
+    return render_template('main/index.html', title='Home', user=user)
