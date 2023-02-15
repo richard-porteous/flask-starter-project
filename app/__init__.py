94ea442c8bc49817242fd57d843881902311ab04
Richard Porteous <rich.porteous@gmail.com>
2023-02-15 14:53:16 -0700

    End of Flesh-Out-Our-App

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 from app.main import bp as main_bp
 app.register_blueprint(main_bp)
