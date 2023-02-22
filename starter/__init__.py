5b94350941c09cdbca26c72417f52207a3147e53
Richard Porteous <rich.porteous@gmail.com>
2023-02-22 15:36:02 -0700

    last bits

--- starter/__init__.py
+++ starter/__init__.py
@@ -3,16 +3,20 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+login = LoginManager(app)
 bootstrap = Bootstrap(app)
 
 from starter.main import bp as main_bp
 app.register_blueprint(main_bp)
+
+from starter.auth import bp as auth_bp
+app.register_blueprint(auth_bp)
 
 from starter.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
