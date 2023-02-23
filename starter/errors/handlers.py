869955726c8c699d5c3b1167ba51a8e6fc17fcc7
Richard Porteous <rich.porteous@gmail.com>
2023-02-23 14:02:01 -0700

    500 error page

--- starter/errors/handlers.py
+++ starter/errors/handlers.py
@@ -4,3 +4,8 @@ from starter.errors import bp
 @bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
+
+
+@bp.app_errorhandler(500)
+def not_found_error(error):
+    return render_template('errors/500.html'), 500
